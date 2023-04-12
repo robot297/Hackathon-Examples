6242bf3ea9f7dd2c363015ff82cb81d5f0743bce
Liz Shockey <lizsdev@gmail.com>
2023-04-11 22:59:20 -0400

    style: :sparkles: finish money movement parameters

--- src/hackathon_helper/api/money_movement.py
+++ src/hackathon_helper/api/money_movement.py
@@ -44,15 +44,25 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def withdraw_money(self, payload):
+    def withdraw_money(self, account_id, amount, party, check_number=None):
         """This will withdraw money from an account.
 
         Args:
-            payload (dict): The contents of the request.
+            account_id (str): The contents of the request.
+            amount (decimal): The amount to deposit in dollars.
+            party (str): The payer name.
+            check_number (str, optional): Defaults to none.
+                The check number if applicable.
 
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "accountID": account_id,
+            "amount": float(amount),
+            "checkNumber": check_number,
+            "party": party
+        }
         activity_response = requests.post(url=self.uat_url + 'activity/deposit',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -60,15 +70,41 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def card_purchase(self, payload):
+    def card_purchase(self, card_number, first_name, last_name, expire_month, expire_year,
+                      zipcode, cvv_code, amount, merchant, merchant_code, in_person = True):
         """This method attempts to create a card purchase and returns a valid receipt.
 
         Args:
-            payload (dict): The contents of the request.
+            card_number (str): a valid credit card number.
+            first_name (str): cardholder's first name.
+            last_name (str): cardholder's last name.
+            expire_month (str): expiration month (MM).
+            expire_year (str): expiration year (YYYY).
+            zipcode (str): Postal code associated with this card.
+            amount (decimal): Purchase amount in dollars.
+            merchant_code (str): The corresponding merchant code.
+            cvv_code (str): Security code.
+            in_person (bool, optional): Default true. 
+                Indicates if purchase was made in-person (true) or online (false)
 
         Returns:
             Response: The response from the api including content and status code.
         """
+
+        payload = {
+            "cardNumber": card_number,
+            "firstName": first_name,
+            "lastName": last_name,
+            "expMonth": expire_month,
+            "expYear": expire_year,
+            "zip": zipcode,
+            "cvv": cvv_code,
+            "amount": float(amount),
+            "merchant": merchant,
+            "mcc": merchant_code,
+            "inPerson": in_person
+        }
+
         activity_response = requests.post(url=self.uat_url + 'activity/card-purchase',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -76,15 +112,24 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def card_purchase_reversal(self, payload):
+    def card_purchase_reversal(self, account_id, transaction_id, reason):
         """This method attempts to reverse an existing card purchase and credits the account.
 
         Args:
-            payload (dict): The contents of the request.
-        
+            account_id (str): The contents of the request.
+            transaction_id (str): the tranaction ID to be reversed.
+            reason (str): Explanation for card reversal.
+                Enum: "BILLING" "DISPUTE" "UNAUTHORIZED"
+
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "accountID": account_id,
+            "transactionID": transaction_id,
+            "reason": reason
+        }
+
         activity_response = requests.post(url=self.uat_url + 'activity/card-reverse',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -92,15 +137,24 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def pay_card(self, payload):
+    def pay_card(self, account_id, amount, check_number=None):
         """This method attempts to pay a credit account according business rules.
 
         Args:
-            payload (dict): The contents of the request.
+            account_id (str): The account id.
+            amount (decimal): The amount to deposit in dollars.
+            check_number (str, optional): Defaults to none.
+                The check number if applicable.
         
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "accountID": account_id,
+            "amount": float(amount),
+            "checkNumber": check_number
+        }
+
         activity_response = requests.post(url=self.uat_url + 'activity/payment',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -108,17 +162,30 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def card_purchase_from_digital_wallet(self, payload):
+    def card_purchase_from_digital_wallet(self, encrypted_card_data, wallet_id, amount,
+                                          merchant, merchant_code):
         """This method attempts to create a card purchase through a digital wallet.
         A separate method can create the encrypted card data stream which is required for
         the digital wallet.
 
         Args:
-            payload (dict): The contents of the request.
+            encrypted_card_data (str): the encrypted card data.
+            wallet_id (str): the digital wallet identifier.
+            ammount (decimal): Purchase amount.
+            merchant (str): The merchant name.
+            merchant_code (str): The corresponding merchant code
         
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "encryptedCardData": encrypted_card_data,
+            "walletID": wallet_id,
+            "merchant": merchant,
+            "mcc": merchant_code,
+            "amount": amount
+        }
+
         activity_response = requests.post(url=self.uat_url + 'activity/digital-wallet',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -126,16 +193,28 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def external_funds_transfer(self, payload):
+    def external_funds_transfer(self, customer_id, account_id, routing_number,
+                                external_account_id, amount):
         """This method moves funds from a specific customer account to a different
         customer account. Only savings or checking accounts are allowed.
 
         Args:
-            payload (dict): The contents of the request.
+            customer_id (str): Customer identifier for the owner of the account.
+            account_id (str): Unique identifier for the account sending funds.
+            routing_number (str): Routing number for external account.
+            external_account_id (str): Unique identifier for the external account receiving funds.
+            amount (decimal): Transfer amount.
         
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "customerID": customer_id,
+            "accountID": account_id,
+            "routingNumber": routing_number,
+            "externalAccountID": external_account_id,
+            "amount": float(amount)
+        }
         activity_response = requests.post(url=self.uat_url + 'activity/external-transfer',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -143,16 +222,26 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def internal_funds_transfer(self, payload):
+    def internal_funds_transfer(self, customer_id, to_account_id, from_account_id, amount):
         """This method moves funds between two different accounts
         for the same customer according business rules.
 
         Args:
-            payload (dict): The contents of the request.
+            customer_id (str): Customer identifier for the owner of the account.
+            to_account_id (str): Unique identifier for the account receiving funds.
+            from_account_id (str): Unique identifier for the account sending funds.
+            amount (decimal): Transfer amount.
         
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "customerID": customer_id,
+            "toAccountID": to_account_id,
+            "fromAccountID": from_account_id,
+            "amount": amount
+        }
+
         activity_response = requests.post(url=self.uat_url + 'activity/funds-transfer',
                                           auth=self.basic_auth,
                                           headers=self.headers,
@@ -160,15 +249,20 @@ class MoneyMovement(APIBuilder):
                                           timeout=60)
         return activity_response
 
-    def create_memo(self, payload):
+    def create_memo(self, account_id, memo):
         """This method notates a specific account with a custom message
 
         Args:
-            payload (dict): The contents of the request.
+            account_id (str): The account id.
+            memo (str): Memo string. (<= 256 characters)
         
         Returns:
             Response: The response from the api including content and status code.
         """
+        payload = {
+            "accountID": account_id,
+            "memo": memo
+        }
         activity_response = requests.post(url=self.uat_url + 'activity/memo',
                                           auth=self.basic_auth,
                                           headers=self.headers,
